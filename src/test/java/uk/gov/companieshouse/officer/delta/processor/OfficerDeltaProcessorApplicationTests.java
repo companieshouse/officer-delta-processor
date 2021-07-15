9c31509120a2e3642b28c80b16cf1a73dfe17fa8
Chris Lewis <clewis1@companieshouse.gov.uk>
2021-07-15 11:59:42 +0100

    Initial push to master for skeleton files

---
+++ src/test/java/uk/gov/companieshouse/officer/delta/processor/OfficerDeltaProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.officer.delta.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficerDeltaProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
