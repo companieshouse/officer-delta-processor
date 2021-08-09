105cf49c964653491702b96334e3cb8859a207a8
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2021-08-09 13:53:21 +0100

    Renamed integration test

--- src/test/java/uk/gov/companieshouse/officer/delta/processor/OfficerDeltaConsumerIT.java
+++ src/test/java/uk/gov/companieshouse/officer/delta/processor/OfficerDeltaConsumerIT.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @ExtendWith(MockitoExtension.class)
-class OfficerDeltaConsumerConsumerIT extends BaseKafkaIntegrationTest {
+class OfficerDeltaConsumerIT extends BaseKafkaIntegrationTest {
     @Test
     void testCanConsumeMessage() {
         // Given
