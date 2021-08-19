ffbfaa0a298c981bd285f5e0f82d5a9ba69605f9
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2021-08-19 15:00:32 +0100

    Make generic name conform to standards

--- src/main/java/uk/gov/companieshouse/officer/delta/processor/processor/Processor.java
+++ src/main/java/uk/gov/companieshouse/officer/delta/processor/processor/Processor.java
@@ -2,6 +2,6 @@ package uk.gov.companieshouse.officer.delta.processor.processor;
 
 import uk.gov.companieshouse.officer.delta.processor.exception.ProcessException;
 
-public interface Processor<InputModel> {
-    void process(InputModel delta) throws ProcessException;
+public interface Processor<I> {
+    void process(I delta) throws ProcessException;
 }
