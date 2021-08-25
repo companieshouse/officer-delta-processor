255f7aed19d6da4c954ce958fd4e10afdf5a7227
Phil Glover <pglover@companieshouse.gov.uk>
2021-08-25 12:52:11 +0100

    add more fields - name, address and ServSameAsRO

--- src/main/java/uk/gov/companieshouse/officer/delta/processor/tranformer/Restructure.java
+++ src/main/java/uk/gov/companieshouse/officer/delta/processor/tranformer/Restructure.java
@@ -19,5 +19,21 @@ public class Restructure implements Transform {
         LocalDateTime outputAppointment = LocalDate.parse(inputAppointed, formatter).atStartOfDay();
 
         outputOfficer.setAppointedOn(outputAppointment);
+        outputOfficer.setCompanyNumber(inputOfficer.getCompanyNumber());
+        outputOfficer.setForename(inputOfficer.getForename());
+        outputOfficer.setOtherForenames(inputOfficer.getMiddleName());
+        outputOfficer.setSurname(inputOfficer.getSurname());
+        outputOfficer.setNationality(inputOfficer.getNationality());
+        outputOfficer.setOccupation(inputOfficer.getNationality());
+
+        outputOfficer.setServiceAddress(inputOfficer.getServiceAddress());
+        outputOfficer.setServiceAddressSameAsRegisteredOfficeAddress(setValueOfServiceAddress(inputOfficer.getServiceAddressSameAsRegisteredAddress()));
+    }
+
+    private boolean setValueOfServiceAddress(final String serviceAddressSameAsRegisteredAddress) {
+        if (serviceAddressSameAsRegisteredAddress.equals("Y")) {
+            return Boolean.valueOf("true");
+        }
+        else return Boolean.valueOf("false");
     }
 }
