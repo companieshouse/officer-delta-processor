11a0341e8f023f222384aebe3a6f470db06b7b68
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2021-09-22 11:40:42 +0100

    Format code

--- src/main/java/uk/gov/companieshouse/officer/delta/processor/tranformer/OfficerTransform.java
+++ src/main/java/uk/gov/companieshouse/officer/delta/processor/tranformer/OfficerTransform.java
@@ -1,15 +1,16 @@
 package uk.gov.companieshouse.officer.delta.processor.tranformer;
 
 
-import static uk.gov.companieshouse.officer.delta.processor.tranformer.TransformerUtils.parseDateString;
-import static uk.gov.companieshouse.officer.delta.processor.tranformer.TransformerUtils.parseDateTimeString;
-import static uk.gov.companieshouse.officer.delta.processor.tranformer.TransformerUtils.parseYesOrNo;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.model.delta.officers.OfficerAPI;
 import uk.gov.companieshouse.officer.delta.processor.exception.ProcessException;
 import uk.gov.companieshouse.officer.delta.processor.model.OfficersItem;
+import uk.gov.companieshouse.officer.delta.processor.model.enums.RolesWithDateOfBirth;
+
+import static uk.gov.companieshouse.officer.delta.processor.tranformer.TransformerUtils.parseDateString;
+import static uk.gov.companieshouse.officer.delta.processor.tranformer.TransformerUtils.parseDateTimeString;
+import static uk.gov.companieshouse.officer.delta.processor.tranformer.TransformerUtils.parseYesOrNo;
 
 @Component
 public class OfficerTransform implements Transformative<OfficersItem, OfficerAPI> {
@@ -27,11 +28,14 @@ public class OfficerTransform implements Transformative<OfficersItem, OfficerAPI
 
     @Override
     public OfficerAPI transform(OfficersItem source, OfficerAPI officer) throws ProcessException {
-        officer.setUpdatedAt(parseDateTimeString("changedAt", source.getChangedAt()));
-        officer.setAppointedOn(parseDateString("appointmentDate", source.getAppointmentDate()));
+        officer.setUpdatedAt(
+                parseDateTimeString("changedAt", source.getChangedAt()));
+        officer.setAppointedOn(
+                parseDateString("appointmentDate", source.getAppointmentDate()));
 
         if (source.getResignationDate() != null) {
-            officer.setResignedOn(parseDateString("resignation_date", source.getResignationDate()));
+            officer.setResignedOn(
+                    parseDateString("resignation_date", source.getResignationDate()));
         }
         officer.setCompanyNumber(source.getCompanyNumber());
         officer.setTitle(source.getTitle());
@@ -40,7 +44,7 @@ public class OfficerTransform implements Transformative<OfficersItem, OfficerAPI
         officer.setSurname(source.getSurname());
         officer.setNationality(source.getNationality());
         officer.setOccupation(source.getOccupation());
-        officer.setDateOfBirth(parseDateString("dateOfBirth", source.getDateOfBirth()));
+
         officer.setOfficerRole(source.getOfficerRole());
         officer.setHonours(source.getHonours());
 
@@ -48,7 +52,8 @@ public class OfficerTransform implements Transformative<OfficersItem, OfficerAPI
         officer.setServiceAddressSameAsRegisteredOfficeAddress(
                 parseYesOrNo(source.getServiceAddressSameAsRegisteredAddress()));
         officer.setUsualResidentialAddress(source.getUsualResidentialAddress());
-        officer.setResidentialAddressSameAsServiceAddress(parseYesOrNo(source.getResidentialAddressSameAsServiceAddress()));
+        officer.setResidentialAddressSameAsServiceAddress(
+                parseYesOrNo(source.getResidentialAddressSameAsServiceAddress()));
         officer.setCountryOfResidence(source.getUsualResidentialCountry());
 
         officer.setIdentificationData(idTransform.transform(source.getIdentification()));
