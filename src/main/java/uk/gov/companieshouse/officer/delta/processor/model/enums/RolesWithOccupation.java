33666f5747e9e0728caa3cac7e283da4d22c9723
Phil Glover <pglover@companieshouse.gov.uk>
2021-09-23 14:54:56 +0100

    add the RolesWithOccupation business rule

---
+++ src/main/java/uk/gov/companieshouse/officer/delta/processor/model/enums/RolesWithOccupation.java
@@ -0,0 +1,39 @@
+package uk.gov.companieshouse.officer.delta.processor.model.enums;
+
+import java.util.EnumSet;
+
+public enum RolesWithOccupation {
+
+    DIRECTOR(OfficerRole.DIR),
+    MEMBER_OF_A_MANAGEMENT_ORGAN(OfficerRole.MEMMANORG),
+    MEMBER_OF_A_SUPERVISORY_ORGAN(OfficerRole.MEMSUPORG),
+    MEMBER_OF_AN_ADMINISTRATIVE_ORGAN(OfficerRole.MEMADMORG),
+    NOMINEE_DIRECTOR(OfficerRole.NOMDIR),
+    NOMINEE_SECRETARY(OfficerRole.NOMSEC),
+    SECRETARY(OfficerRole.SEC);
+
+    private final OfficerRole officerRole;
+
+    RolesWithOccupation(OfficerRole officerRole) {
+        this.officerRole = officerRole;
+    }
+
+    public String getOfficerRole() {
+        return officerRole.getValue();
+    }
+
+    public static boolean includes(final OfficerRole role) {
+        return includes(role.getValue());
+    }
+
+    public static boolean includes(final String role) {
+        return EnumSet.allOf(RolesWithOccupation.class).stream()
+            .map(r -> r.officerRole.getValue())
+            .anyMatch(role::equals);
+    }
+}
+
+
+
+
+
