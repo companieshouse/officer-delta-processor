68bd1dabd27c64d43f0e836ba24569b668a63011
nghessen-ch <nghessen@companieshouse.gov.uk>
2025-07-17 10:57:30 +0100

    adding flat to dockerstart file

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -3,4 +3,4 @@
 # Start script for officer-delta-processor
 
 PORT=8080
-exec java -jar -Dserver.port="${PORT}" "officer-delta-processor.jar"
+exec java -jar -Dserver.port="${PORT}" -XX:MaxRAMPercentage=80 "officer-delta-processor.jar"
