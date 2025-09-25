d55dadfe0b9f522cd4526510cefeafb6d34bafbf
dhawes-ch <dhawes@companieshouse.gov.uk>
2025-09-25 16:46:57 +0100

    SIV-771 - Align code style (#121)
    * Code style
    
    * Indicate nullability on model

--- src/main/java/uk/gov/companieshouse/officer/delta/processor/model/DeltaIdentityVerificationDetails.java
+++ src/main/java/uk/gov/companieshouse/officer/delta/processor/model/DeltaIdentityVerificationDetails.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.util.List;
 import java.util.Objects;
+import org.springframework.lang.Nullable;
 
 /**
  * The type Delta identity verification details.
@@ -14,93 +15,109 @@ import java.util.Objects;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class DeltaIdentityVerificationDetails {
 
+    @Nullable
     @JsonProperty("anti_money_laundering_supervisory_bodies")
     private List<String> antiMoneyLaunderingSupervisoryBodies;
 
+    @Nullable
     @JsonProperty("appointment_verification_end_on")
     private String appointmentVerificationEndOn;
 
+    @Nullable
     @JsonProperty("appointment_verification_statement_date")
     private String appointmentVerificationStatementDate;
 
+    @Nullable
     @JsonProperty("appointment_verification_statement_due_on")
     private String appointmentVerificationStatementDueOn;
 
+    @Nullable
     @JsonProperty("appointment_verification_start_on")
     private String appointmentVerificationStartOn;
 
+    @Nullable
     @JsonProperty("authorised_corporate_service_provider_name")
     private String authorisedCorporateServiceProviderName;
 
+    @Nullable
     @JsonProperty("identity_verified_on")
     private String identityVerifiedOn;
 
+    @Nullable
     @JsonProperty("preferred_name")
     private String preferredName;
 
 
 
+    @Nullable
     public List<String> getAntiMoneyLaunderingSupervisoryBodies() {
         return antiMoneyLaunderingSupervisoryBodies;
     }
 
-    public void setAntiMoneyLaunderingSupervisoryBodies(List<String> antiMoneyLaunderingSupervisoryBodies) {
+    public void setAntiMoneyLaunderingSupervisoryBodies(@Nullable List<String> antiMoneyLaunderingSupervisoryBodies) {
         this.antiMoneyLaunderingSupervisoryBodies = antiMoneyLaunderingSupervisoryBodies;
     }
 
+    @Nullable
     public String getAppointmentVerificationEndOn() {
         return appointmentVerificationEndOn;
     }
 
-    public void setAppointmentVerificationEndOn(String appointmentVerificationEndOn) {
+    public void setAppointmentVerificationEndOn(@Nullable String appointmentVerificationEndOn) {
         this.appointmentVerificationEndOn = appointmentVerificationEndOn;
     }
 
+    @Nullable
     public String getAppointmentVerificationStatementDate() {
         return appointmentVerificationStatementDate;
     }
 
-    public void setAppointmentVerificationStatementDate(String appointmentVerificationStatementDate) {
+    public void setAppointmentVerificationStatementDate(@Nullable String appointmentVerificationStatementDate) {
         this.appointmentVerificationStatementDate = appointmentVerificationStatementDate;
     }
 
+    @Nullable
     public String getAppointmentVerificationStatementDueOn() {
         return appointmentVerificationStatementDueOn;
     }
 
-    public void setAppointmentVerificationStatementDueOn(String appointmentVerificationStatementDueOn) {
+    public void setAppointmentVerificationStatementDueOn(@Nullable String appointmentVerificationStatementDueOn) {
         this.appointmentVerificationStatementDueOn = appointmentVerificationStatementDueOn;
     }
 
+    @Nullable
     public String getAppointmentVerificationStartOn() {
         return appointmentVerificationStartOn;
     }
 
-    public void setAppointmentVerificationStartOn(String appointmentVerificationStartOn) {
+    public void setAppointmentVerificationStartOn(@Nullable String appointmentVerificationStartOn) {
         this.appointmentVerificationStartOn = appointmentVerificationStartOn;
     }
 
+    @Nullable
     public String getAuthorisedCorporateServiceProviderName() {
         return authorisedCorporateServiceProviderName;
     }
 
-    public void setAuthorisedCorporateServiceProviderName(String authorisedCorporateServiceProviderName) {
+    public void setAuthorisedCorporateServiceProviderName(@Nullable String authorisedCorporateServiceProviderName) {
         this.authorisedCorporateServiceProviderName = authorisedCorporateServiceProviderName;
     }
 
+    @Nullable
     public String getIdentityVerifiedOn() {
         return identityVerifiedOn;
     }
 
-    public void setIdentityVerifiedOn(String identityVerifiedOn) {
+    public void setIdentityVerifiedOn(@Nullable String identityVerifiedOn) {
         this.identityVerifiedOn = identityVerifiedOn;
     }
 
+    @Nullable
     public String getPreferredName() {
         return preferredName;
     }
 
-    public void setPreferredName(String preferredName) {
+    public void setPreferredName(@Nullable String preferredName) {
         this.preferredName = preferredName;
     }
 
--- src/main/java/uk/gov/companieshouse/officer/delta/processor/transformer/IdentityVerificationDetailsTransform.java
+++ src/main/java/uk/gov/companieshouse/officer/delta/processor/transformer/IdentityVerificationDetailsTransform.java
@@ -1,16 +1,21 @@
 package uk.gov.companieshouse.officer.delta.processor.transformer;
 
+import static uk.gov.companieshouse.officer.delta.processor.transformer.TransformerUtils.parseLocalDate;
+
+import java.util.Optional;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.appointment.IdentityVerificationDetails;
 import uk.gov.companieshouse.officer.delta.processor.exception.NonRetryableErrorException;
 import uk.gov.companieshouse.officer.delta.processor.model.DeltaIdentityVerificationDetails;
 
-import java.util.function.Consumer;
-
-import static uk.gov.companieshouse.officer.delta.processor.transformer.TransformerUtils.parseLocalDate;
-
 @Component
 public class IdentityVerificationDetailsTransform implements Transformative<DeltaIdentityVerificationDetails, IdentityVerificationDetails>{
+    private static final String IDENTITY_VERIFIED_ON = "identity_verified_on";
+    private static final String APPOINTMENT_VERIFICATION_START_ON = "appointment_verification_start_on";
+    private static final String APPOINTMENT_VERIFICATION_STATEMENT_DUE_ON = "appointment_verification_statement_due_on";
+    private static final String APPOINTMENT_VERIFICATION_STATEMENT_DATE = "appointment_verification_statement_date";
+    private static final String APPOINTMENT_VERIFICATION_END_ON = "appointment_verification_end_on";
+
     @Override
     public IdentityVerificationDetails factory() {
         return new IdentityVerificationDetails();
@@ -21,43 +26,22 @@ public class IdentityVerificationDetailsTransform implements Transformative<Delt
             throws NonRetryableErrorException {
         if (source == null) return output;
 
-        setIfNotNull(output::setAntiMoneyLaunderingSupervisoryBodies, source.getAntiMoneyLaunderingSupervisoryBodies());
+        Optional<String> appointmentVerificationEndOn = Optional.ofNullable(source.getAppointmentVerificationEndOn());
+        Optional<String> appointmentVerificationStatementDate = Optional.ofNullable(source.getAppointmentVerificationStatementDate());
+        Optional<String> appointmentVerificationStatementDueOn = Optional.ofNullable(source.getAppointmentVerificationStatementDueOn());
+        Optional<String> appointmentVerificationStartOn = Optional.ofNullable(source.getAppointmentVerificationStartOn());
+        Optional<String> identityVerifiedOn = Optional.ofNullable(source.getIdentityVerifiedOn());
 
-        if (source.getAppointmentVerificationEndOn() != null) {
-            setIfNotNull(output::setAppointmentVerificationEndOn,
-                    parseLocalDate("appointment_verification_end_on", source.getAppointmentVerificationEndOn()));
-        }
+        appointmentVerificationEndOn.ifPresent(dateString -> output.setAppointmentVerificationEndOn(parseLocalDate(APPOINTMENT_VERIFICATION_END_ON, dateString)));
+        appointmentVerificationStatementDate.ifPresent(dateString -> output.setAppointmentVerificationStatementDate(parseLocalDate(APPOINTMENT_VERIFICATION_STATEMENT_DATE, dateString)));
+        appointmentVerificationStatementDueOn.ifPresent(dateString -> output.setAppointmentVerificationStatementDueOn(parseLocalDate(APPOINTMENT_VERIFICATION_STATEMENT_DUE_ON, dateString)));
+        appointmentVerificationStartOn.ifPresent(dateString -> output.setAppointmentVerificationStartOn(parseLocalDate(APPOINTMENT_VERIFICATION_START_ON, dateString)));
+        identityVerifiedOn.ifPresent(dateString -> output.setIdentityVerifiedOn(parseLocalDate(IDENTITY_VERIFIED_ON, dateString)));
 
-        if (source.getAppointmentVerificationStatementDate() != null) {
-            setIfNotNull(output::setAppointmentVerificationStatementDate,
-                    parseLocalDate("appointment_verification_statement_date", source.getAppointmentVerificationStatementDate()));
-        }
-
-        if (source.getAppointmentVerificationStatementDueOn() != null) {
-            setIfNotNull(output::setAppointmentVerificationStatementDueOn,
-                    parseLocalDate("appointment_verification_statement_due_on", source.getAppointmentVerificationStatementDueOn()));
-        }
-
-        if (source.getAppointmentVerificationStartOn() != null) {
-            setIfNotNull(output::setAppointmentVerificationStartOn,
-                    parseLocalDate("appointment_verification_start_on", source.getAppointmentVerificationStartOn()));
-        }
-
-        setIfNotNull(output::setAuthorisedCorporateServiceProviderName, source.getAuthorisedCorporateServiceProviderName());
-
-        if (source.getIdentityVerifiedOn() != null) {
-            setIfNotNull(output::setIdentityVerifiedOn,
-                    parseLocalDate("identity_verified_on", source.getIdentityVerifiedOn()));
-        }
-
-        setIfNotNull(output::setPreferredName, source.getPreferredName());
+        output.setAuthorisedCorporateServiceProviderName(source.getAuthorisedCorporateServiceProviderName());
+        output.setAntiMoneyLaunderingSupervisoryBodies(source.getAntiMoneyLaunderingSupervisoryBodies());
+        output.setPreferredName(source.getPreferredName());
 
         return output;
-    }
-
-    private <T> void setIfNotNull(Consumer<T> setter, T value) {
-        if (value != null) {
-            setter.accept(value);
-        }
     }
 }
