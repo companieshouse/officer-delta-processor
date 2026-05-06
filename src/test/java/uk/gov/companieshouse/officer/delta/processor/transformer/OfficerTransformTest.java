699de5aba363d55ec1789fa1f8ae135c24b80e25
mwestacott <mwestacott2@companieshouse.gov.uk>
2026-05-06 14:18:47 +0100

    LP-1911 Add test to validate addresses for non-corporate partners

--- src/test/java/uk/gov/companieshouse/officer/delta/processor/transformer/OfficerTransformTest.java
+++ src/test/java/uk/gov/companieshouse/officer/delta/processor/transformer/OfficerTransformTest.java
@@ -408,16 +408,17 @@ class OfficerTransformTest {
 
     @ParameterizedTest
     @EnumSource(value = OfficerRole.class, names = {"LPLIMPARTCORP", "LPGENPARTCORP"})
-    void testPrincipalOfficeAddressIsSetWhenCorporatePartnerIsTransformed(OfficerRole officerRole) throws NonRetryableErrorException {
+    void testCorrectAddressesAreSetWhenCorporatePartnerIsTransformed(OfficerRole officerRole) throws NonRetryableErrorException {
         Data officerAPI = testTransform.factory();
         OfficersItem officer = createOfficer(addressAPI, identification);
-        AddressAPI principalOfficeAddressAPI = new AddressAPI();
 
         officer.setKind(officerRole.name());
-        officer.setPrincipalOfficeAddress(principalOfficeAddressAPI);
-
         officer.setAppointmentDate(VALID_DATE);
         officer.setDateOfBirth(VALID_DATE);
+
+        officer.setServiceAddress(null);
+        AddressAPI principalOfficeAddressAPI = new AddressAPI();
+        officer.setPrincipalOfficeAddress(principalOfficeAddressAPI);
 
         when(identificationTransform.transform(identification)).thenReturn(identificationAPI);
         when(principalOfficeAddressTransform.transform(principalOfficeAddressAPI)).thenReturn(principalOfficeAddress);
@@ -425,6 +426,29 @@ class OfficerTransformTest {
         final Data result = testTransform.transform(officer, officerAPI);
 
         assertThat(result.getPrincipalOfficeAddress(), is(principalOfficeAddress));
+        assertThat(result.getServiceAddress(), is(nullValue()));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = OfficerRole.class, names = {"LPLIMPART", "LPGENPART"})
+    void testCorrectAddressesAreSetWhenNaturalPersonPartnerIsTransformed(OfficerRole officerRole) throws NonRetryableErrorException {
+        Data officerAPI = testTransform.factory();
+        OfficersItem officer = createOfficer(addressAPI, identification);
+
+        officer.setKind(officerRole.name());
+        officer.setAppointmentDate(VALID_DATE);
+        officer.setDateOfBirth(VALID_DATE);
+
+        AddressAPI principalOfficeAddressAPI = new AddressAPI();
+        officer.setPrincipalOfficeAddress(principalOfficeAddressAPI);
+
+        when(identificationTransform.transform(identification)).thenReturn(identificationAPI);
+        when(serviceAddressTransform.transform(addressAPI)).thenReturn(serviceAddress);
+
+        final Data result = testTransform.transform(officer, officerAPI);
+
+        assertThat(result.getPrincipalOfficeAddress(), is(nullValue()));
+        assertThat(result.getServiceAddress(), is(sameInstance(serviceAddress)));
     }
 
     private void verifyProcessingError(final Data data, final OfficersItem officer,
